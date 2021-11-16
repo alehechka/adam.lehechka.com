c827b9c5a3d1bfda23104007e7dd4b283a0513e7
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2021-11-15 21:08:17 -0600

    Page-Component Refactor (#6)
    * create markdown/contentlist components
    
    * refactor/create articles/notes pages
    
    * notes file

---
+++ content/notes/home-server.md
@@ -0,0 +1,4 @@
+---
+title: 'Home Server'
+date: '2020-11-14'
+---
--- pages/notes/index.tsx
+++ pages/notes/index.tsx
@@ -1,5 +1,18 @@
-const Notes = () => {
-	return <h1>Notes</h1>;
+import { InferGetStaticPropsType, GetStaticProps } from 'next';
+import { getSortedContentData } from '@lib/contents';
+import ContentList from '@components/ContentList';
+
+export const getStaticProps: GetStaticProps = async () => {
+	const allContentData = getSortedContentData('notes');
+	return {
+		props: {
+			allContentData,
+		},
+	} as const;
+};
+
+const Notes = ({ allContentData }: InferGetStaticPropsType<typeof getStaticProps>) => {
+	return <ContentList allContentData={allContentData} pathPrefix='notes' title='Notes' />;
 };
 
 export default Notes;
